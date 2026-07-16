782c1cf61fb985096d2e880c6344bad7b20ce515
Michael Feist <maxxus220@gmail.com>
2026-07-16 18:45:13 -0500

    nix: Add flake.nix with rust deps

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "traversal – tag cross-referencer";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/1e78637806c14b81a1e8dccadf00be7e93dda457";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+            clippy
+            rustc
+            rustfmt
+          ];
+
+          RUST_EDITION = "2024";
+        };
+      });
+}
